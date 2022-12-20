afa562e5b33d9e22e2f2f4b16baee88782174d9c
Evgenii Popov <saip0904@gmail.com>
2022-12-20 10:19:19 +0500

    Создали папку лекции 1

---
+++ Lesson 1/Examples001_Square/Program.cs
@@ -0,0 +1,5 @@
+﻿//Напишите прогрмму, которая на вход принимает число и выдает его квадрат (число умноженное на само себя)
+Console.WriteLine("Ввведите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number * number;
+Console.WriteLine("Квадрат числа " + number + " равен " + result);
